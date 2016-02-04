8fdf5253cf0f5f3914fd20f5ddd6c8b3a2f4c32d
L1n <490772448@qq.com>
2016-02-04 14:44:26 +0800

    Windows Only

--- Python项目开发实战/第2章 Python脚本/2.4 使用ctypes和pywin32访问原生API/ctypes_learn.py
+++ Python项目开发实战/第2章 Python脚本/2.4 使用ctypes和pywin32访问原生API/ctypes_learn.py
@@ -13,6 +13,14 @@ def main():
     libc.printf(b"%d %s %s hanging on a wall\n", 6, b"green", b"bottles")
     libc.printf(b"Pi is: %f\n", ct.c_double(3.14159))
 
+    d = ct.c_int()
+    print(d.value)
+    print(libc.sscanf(b"6",b"%d",ct.byref(d)))
+    print(d.value)
+
+    # 这个函数返回Windows系统上一个可用的驱动器列表，这一点使用Python标准库是不容易实现的。
+    # 唯一复杂的地方是返回的列表是一个位掩码。
+
 
 if __name__ == "__main__":
     main()
