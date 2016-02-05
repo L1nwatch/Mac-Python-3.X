b0da53b69c47bb2bbfb2f0f9266e4330cce31161
L1n <490772448@qq.com>
2016-02-05 19:34:28 +0800

    pywin32 学习, Windows Only

---
+++ Python项目开发实战/第2章 Python脚本/2.4 使用ctypes和pywin32访问原生API/toolhireCOM.py
@@ -0,0 +1,15 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# version: Python3.X
+''' 演示了在Python中如何使用Excel COM接口打开一个应用并让用户选择文件.
+Windows Only, pywin32下载地址:http://www.lfd.uci.edu/~gohlke/pythonlibs/#pywin32
+'''
+__author__ = '__L1n__w@tch'
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
