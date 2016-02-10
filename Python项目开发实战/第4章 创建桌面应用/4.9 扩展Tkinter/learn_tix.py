c104b082d54a8ec75cb3dedfcbf49f07dc29e513
L1n <490772448@qq.com>
2016-02-10 17:18:21 +0800

    Windows Only,Tix库的示例代码

--- Python项目开发实战/第4章 创建桌面应用/4.9 扩展Tkinter/learn_tix.py
+++ Python项目开发实战/第4章 创建桌面应用/4.9 扩展Tkinter/learn_tix.py
@@ -3,6 +3,8 @@
 # version: Python3.X
 ''' 了解的小部件包括ComboBox、ScrolledText和Notebook
 直接在Mac下运行不了,好像是得安装tix组件?
+Mac 参考:http://stackoverflow.com/questions/27751923/tkinter-tclerror-cant-find-package-tix
+说是tix已经落伍了,推荐使用Ttk,所以这个在Mac下运行不了我也不去解决了
 Windows下运行成功了
 '''
 __author__ = '__L1n__w@tch'
