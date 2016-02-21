5e91f6c5e28efb391129b61b1efa07baf05a7d49
L1n <490772448@qq.com>
2016-02-21 10:40:03 +0800

    主要是为了在实验吧上记笔记用的, 实验吧的那个笔记不让复制, 只好从源码下手, 但是源码里面有些乱七八糟的标签所以就写程序来格式化一下

--- shiyanbar/clear_tag.py
+++ shiyanbar/clear_tag.py
@@ -1,19 +1,30 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*-
 # version: Python3.X
-''' 用于清除文件中的某一指定标签
+'''
+    1. 用于清除文件中的某一指定标签
+    2. 清除连续的两个回车符
 '''
 __author__ = '__L1n__w@tch'
 
 import re
 
 
+def delete_double_newline(text):
+    """
+    :param text: "aaa\n\naaa"
+    :return: "aaa\naaa"
+    """
+    text = re.sub("\n\n", "\n", text)
+    return text
+
+
 def delete_pairs_tags(response, html_tag="script"):
     """
     清除成对的标签及其中的内容
 
-    :param response: "3.1 打开Brute Froce页面，在页面的最下方点击View Source。。查看源代码。如图11所示
-<center>![](/UploadImage/2016/1/4/20_95653)</center><center>图 11</center>"
+    :param response: '''3.1 打开Brute Froce页面，在页面的最下方点击View Source。。查看源代码。如图11所示
+    <center>![](/UploadImage/2016/1/4/20_95653)</center><center>图 11</center>'''
     :param html_tag: "center"
     :return: "3.1 打开Brute Froce页面，在页面的最下方点击View Source。。查看源代码。如图11所示"
     """
@@ -27,6 +38,7 @@ def main():
     with open(file_name, "r") as f:
         data = f.read()
         data = delete_pairs_tags(data, "center")
+        data = delete_double_newline(data)
 
     with open(file_name, "w") as f:
         f.write(data)
