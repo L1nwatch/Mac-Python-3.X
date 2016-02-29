33eeb48dd18a9bc68d3a1351af61e57a952497f4
L1n <490772448@qq.com>
2016-02-29 20:23:34 +0800

    Test .gitignore

--- 恶意代码分析/第一周/任务一/impHash.py
+++ 恶意代码分析/第一周/任务一/impHash.py
@@ -5,16 +5,40 @@
 
 参考文章: https://www.fireeye.com/blog/threat-research/2014/01/tracking-malware-import-hashing.html
 pefile库-Python2: https://github.com/erocarrera/pefile
-pefile库-Python3: https://github.com/simonzack/pefile-py3k
+pefile库-Python3: https://github.com/XeroNicHS/pefile_py3
 
+实现思路有 2 个:
+第一个直接调用 pefile 库的 get_imphash() 实现
+第二个是根据给定的 rva 范围, 对文件对应的 rva 范围字节流进行哈希
 '''
 __author__ = '__L1n__w@tch'
 
 import pefile
+import hashlib
+
+
+def get_imphash():
+    """
+    仅仅是测试一下如何调用 pefile 库来获取 imphash
+    :return:
+    """
+    pe = pefile.PE("Lab06-02.exe")
+    print(pe.get_imphash())
+
+
+def get_hash(file_name):
+    file_name = "Lab06-02.exe"
+    with open(file_name, "rb") as f:
+        data = f.read()
+        print(len(data))
 
 
 def main():
-    pefile.PE()
+    file_name = ""
+
+    get_imphash()
+    get_hash(None)
+    # get_hash(rva_start, rva_end)
 
 
 if __name__ == "__main__":
