e65eca7fec95872d9ce799f1499db047ad137ccf
L1n <490772448@qq.com>
2016-03-10 13:33:16 +0800

    图书馆登录, 原以为旧的方式用不了了暂时写了个新的, 结果隔了一个上午回来发现旧的可以用了, 就没继续写新的了

--- Others/library_wifi.py
+++ Others/library_wifi.py
@@ -10,7 +10,11 @@ import tkinter.messagebox
 import tkinter
 
 
-def main():
+def old_login():
+    """
+    2016.03.10 还可以用的登录方式, 隔了一天就不能用了...
+    :return:
+    """
     tk = tkinter.Tk()
     tk.withdraw()  # 去掉空窗口
 
@@ -19,8 +23,47 @@ def main():
     response = requests.post(url, post_data)
     if "62208306347465" in response.text:
         tkinter.messagebox.showinfo(title="登录校园网", message="登录成功")
+        return True
     else:
         tkinter.messagebox.showerror(title="登录校园网", message="登录失败,未知错误: {}".format(response.text))
+        return False
+
+
+def new_login():
+    """
+    2016.03.10 测试
+    :return:
+    """
+    url = "http://192.168.253.2/portal/pws?t=li"
+    #     post_data = {userName=13030110024
+    # userPwd=Zm9yZWFjaGxm
+    # serviceTypeHIDE=
+    # serviceType=
+    # userurl=
+    # userip=
+    # basip=
+    # language=Chinese
+    # usermac=null
+    # wlannasid=
+    # entrance=null
+    # portalProxyIP=192.168.253.2
+    # portalProxyPort=50200
+    # dcPwdNeedEncrypt=1
+    # assignIpType=0
+    # appRootUrl=http%3A%2F%2F192.168.253.2%2Fportal%2F
+    # manualUrl=
+    # manualUrlEncryptKey=
+    # }
+    s = requests.Session()
+    response = s.get(url)
+    post_data = {"userName": "13030110024", "userPwd": "Zm9yZWFjaGxm"}
+    response = s.post(url, post_data)
+    print(response.text)
+
+
+def main():
+    if old_login() is False:
+        new_login()
 
 
 if __name__ == "__main__":
