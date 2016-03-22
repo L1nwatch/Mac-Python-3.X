baf6d7ba276d3d134bae6d32aea498edeb9b4396
L1n <490772448@qq.com>
2016-03-22 20:21:17 +0800

    打算自己写一个能够添加文件头的类,现在是测试,主要是实验吧的一道题要求添加 gif 的头才写的这个

---
+++ shiyanbar/misc/FileRepair.py
@@ -0,0 +1,48 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# version: Python3.X
+''' url: http://www.shiyanbar.com/ctf/42
+题目描述:
+看格式这个图片是GIF的
+解题链接： http://ctf5.shiyanbar.com:8080/stega/gif.gif
+
+看了 wp 之后知道是文件头缺失,但是我并没有找到有用的工具能来帮我修复文件头的,所以我考虑自己手写一个
+参考的文件头列表: http://blog.csdn.net/guoguo1980/article/details/5317665
+'''
+__author__ = '__L1n__w@tch'
+
+import binascii
+
+header_dict = {
+    "jpg": "FFD8FF",
+    "png": "89504E47",
+    "gif": "47494638",
+    "tif": "49492A00",
+    "bmp": "424D"  # TODO: 还有很多文件头没收录,参考开头注释中的文件头列表
+}
+
+
+class FileRepair:
+    @staticmethod
+    def add_header(file_name, type):
+        """
+        往一个文件添加指定类型的文件头
+        :param file_name: str()
+        :param type: "gif"
+        :return: "fixed_{file_name}.{type}"
+        """
+        with open(file_name, "rb") as f:
+            data = f.read()
+        new_file_name = "fixed_" + file_name
+        with open(new_file_name, "wb") as f:
+            f.write(binascii.unhexlify(header_dict[type]))
+            f.write(data)
+        return new_file_name
+
+
+def main():
+    FileRepair.add_header("test.gif", "gif")
+
+
+if __name__ == "__main__":
+    main()
