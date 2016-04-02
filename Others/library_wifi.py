6a4ec3027045347a44f8480c2698c0b5dc639d6f
L1n <490772448@qq.com>
2016-04-02 13:23:39 +0800

    登录校园网的程序, 最近校园网认证系统升级了, 所以我这里也写了个升级版的登录程序(后期考虑加入利用 scapy 库自动获取 IP 等)

--- Others/library_wifi.py
+++ Others/library_wifi.py
@@ -11,7 +11,7 @@ import tkinter
 import subprocess
 
 
-def old_login():
+def old_login_before_update():
     """
     2016.03.10 还可以用的登录方式, 隔了一天就不能用了...
     :return:
@@ -27,7 +27,7 @@ def old_login():
         return False
 
 
-def new_login():
+def new_login_before_update():
     """
     2016.03.10 测试
     :return:
@@ -45,7 +45,7 @@ def new_login():
         return False
 
 
-def lab_login():
+def lab_login_before_update():
     """
     在 F520 登录地点
     :return:
@@ -69,16 +69,39 @@ def ping_success():
     return res == 0
 
 
-def main():
-    tk = tkinter.Tk()
-    tk.withdraw()  # 去掉空窗口
+def login_before_update():
+    """
+    某电于 2016 年 3 月 30 号对校园网认证系统进行升级, 这是升级之前的登录方式
+    :return:
+    """
+    if not old_login_before_update() and not new_login_before_update():
+        lab_login_before_update()
 
-    tkinter.messagebox.showerror("啊啊啊啊啊", "自从认证系统更新后我还没有时间写这个程序")
-    exit()
 
-    if not old_login() and not new_login():
-        lab_login()
+def login_after_update():
+    """
+    尝试对新的认证系统进行登录操作
+    :return:
+    """
+    # 模拟访问登陆界面
+    url = "http://10.255.44.33:804/srun_portal_pc.php?ac_id=1&"
+    session = requests.Session()
+    session.get(url)
+
+    # 认证页面
+    url = "http://10.255.44.33:804/include/auth_action.php"
+    post_data = {"action": "login", "username": "13030110024", "password": "foreachlf",
+                 "ac_id": "1"}  # &user_ip=&nas_ip=&user_mac=&save_me=0&ajax=1"}
+    response = session.post(url, post_data)
+    if ping_success():
+        tkinter.messagebox.showinfo("登录校园网", "登录成功")
+    else:
+        tkinter.messagebox.showerror("登录校园网", response.text)
 
 
 if __name__ == "__main__":
-    main()
+    tk = tkinter.Tk()
+    tk.withdraw()  # 去掉空窗口
+    # login_before_update()
+
+    login_after_update()
