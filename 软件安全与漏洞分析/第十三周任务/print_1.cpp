465b0d559bbd9c5eaf77895909e90df80c44d75b
L1n <490772448@qq.com>
2016-05-24 14:55:08 +0800

    格式化串漏洞的两个本机小示例

---
+++ 软件安全与漏洞分析/第十三周任务/print_1.cpp
@@ -0,0 +1,6 @@
+#include "stdio.h"
+void main(void) {
+	int a = 44, b = 77;
+	printf("a=%d, b=%d \n", a, b);
+	printf("a=%d, b=%d\n");
+}
---
+++ 软件安全与漏洞分析/第十三周任务/print_2.cpp
@@ -0,0 +1,8 @@
+#include "stdio.h"
+
+int main(int argc, char ** argv) {
+	int len_print = 0;
+	print("%d\n", len_printf);
+	printf("aaaa%n\n", len_print, &len_print);
+	print("afger write: length=%d\n", len_print);
+}
