769720d6229416d74ce612bfb344bb67dbac498a
L1n <490772448@qq.com>
2016-06-08 16:38:50 +0800

    pyramid 介绍篇节中的代码

--- Pyramid 学习/入门学习/2/first_pyramid.py
+++ Pyramid 学习/入门学习/2/first_pyramid.py
@@ -8,7 +8,25 @@ __author__ = '__L1n__w@tch'
 from pyramid.response import Response
 from pyramid.config import Configurator
 
-pass
+# 单文件应用 - 创建第一个 Pyramid 学习
+# 访问: http://localhost:12309/hello/test
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+
+
+def hello_world(request):
+    return Response("Hello {}!".format(request.matchdict["name"]))
+    # return Response("Hello %(name)s!" % request.matchdict)
+
+
+if __name__ == "__main__":
+    config = Configurator()
+    config.add_route("hello", "/hello/{name}")
+    config.add_view(hello_world, route_name="hello")
+    app = config.make_wsgi_app()
+    server = make_server("0.0.0.0", 12309, app)
+    server.serve_forever()
 
 """
 # 冗长
@@ -151,25 +169,4 @@ from pyramid.response import Response
 @view_config(route_name="fred")
 def fred_view(request):
     return Response("fred")
-"""
-
-"""
-# 单文件应用 - 创建第一个 Pyramid 学习
-
-from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
-from pyramid.response import Response
-
-
-def hello_world(request):
-    return Response("Hello %(name)s!" % request.matchdict)
-
-
-if __name__ == "__main__":
-    config = Configurator()
-    config.add_route("hello", "/hello/{name}")
-    config.add_view(hello_world, route_name="hello")
-    app = config.make_wsgi_app()
-    server = make_server("0.0.0.0", 12309, app)
-    server.serve_forever()
 """
