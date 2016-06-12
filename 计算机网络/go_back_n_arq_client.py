d94f8b37cddb7424ae70f41c13f66f4248dc4160
L1n <490772448@qq.com>
2016-06-12 15:55:48 +0800

    计网大作业之一: 回退 N 帧 ARQ 协议的客户端

--- 计算机网络/go_back_n_arq_client.py
+++ 计算机网络/go_back_n_arq_client.py
@@ -1,7 +1,7 @@
 #!/bin/env python3
 # -*- coding: utf-8 -*-
 # version: Python3.X
-''' 计网大作业之一, 要求实现:后退 N 帧 ARQ 协议, 这是客户端, 负责发包
+""" 计网大作业之一, 要求实现:后退 N 帧 ARQ 协议, 这是客户端, 负责发包
 
 具体要求：
     Done: 要求能模拟2个终端进行数据通信，在通信过程中执行流控+差控协议的所有过程；
@@ -10,15 +10,15 @@
     Done: 要求数据帧编号为0~15；
     Done: 要求窗口滑动能可视化，即可以直观看到数据发送接收时窗口的滑动过程；
     Done: 模拟通信可以用多种手段实现，例如进程间通信、socket、共享内存等；
-'''
-__author__ = '__L1n__w@tch'
-
+"""
 import socket
 import simplejson
 import random
 import bisect
 import time
 import argparse
+
+__author__ = '__L1n__w@tch'
 
 HOST, PORT = "localhost", 23339
 # 模拟发包的类型
@@ -54,8 +54,8 @@ class Sender:
     │{}│{}│{}│{}│{}│{}│{}│{}│{}│{}│{}│{}│{}│{}│{}│{}│
     └──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┴──┘"""
 
-    def __init__(self, sock):
-        self.sock = sock
+    def __init__(self, sock2server):
+        self.sock = sock2server
         self.frame_number = 0
         self.packet_counts = 0  # 用来计数收发包的数量
         self.window_size = SEND_WINDOW_SIZE  # 发送窗口大小
@@ -123,7 +123,6 @@ class Sender:
     def receive_packet(self):
         """
         负责用来收包的函数
-        :param sock: 套接字
         :return: dict()
         """
         sock.settimeout(TIMEOUT * 2.5)
@@ -161,7 +160,7 @@ class Sender:
         while True:
             try:  # 超时重传控制
                 received_data = self.receive_packet()
-            except socket.timeout as e:
+            except socket.timeout:
                 self.paint()  # 可视化打印
                 print("[*] 超时重传; [!] 发送帧{}".format(self.get_current_window_frame_number()))
                 self.send_window_data(self.frame_number)  # 将发送窗口中的帧发送出去
@@ -187,39 +186,38 @@ class Sender:
                 raise RuntimeError("[*] 收包不正常")
 
 
-def add_arguments(parser):
-    parser.add_argument("--verbose", "-v", action="store_true", help="是否显示详细信息, 默认不显示")
-    parser.add_argument("--probability", "-p", default="70:20:10", type=str,
-                        help="设定发包概率, 格式:成功:误码:丢包, 默认值示例:70:20:10")
-    parser.add_argument("--size", "-s", default=3, type=int, help="设定接收窗口大小, 默认值为 3")
-    parser.add_argument("--timeout", "-t", default=2, type=float,
-                        help="设置时延, 发包间隔 TIMEOUT 秒, 超时等待 TIMEOUT * 2.5 秒. 默认值为 2.0")
+def add_arguments(arg_parser):
+    arg_parser.add_argument("--verbose", "-v", action="store_true", help="是否显示详细信息, 默认不显示")
+    arg_parser.add_argument("--probability", "-p", default="70:20:10", type=str,
+                            help="设定发包概率, 格式:成功:误码:丢包, 默认值示例:70:20:10")
+    arg_parser.add_argument("--size", "-s", default=3, type=int, help="设定接收窗口大小, 默认值为 3")
+    arg_parser.add_argument("--timeout", "-t", default=2, type=float,
+                            help="设置时延, 发包间隔 TIMEOUT 秒, 超时等待 TIMEOUT * 2.5 秒. 默认值为 2.0")
 
 
-def set_arguments(opts):
+def set_arguments(options):
     """
     用来将参数传递给全局变量的, 同时传递之前会进行安全检测
-    :param parser: 解析器
-    :param opts: 通过命令行获取的参数
+    :param options: 通过命令行获取的参数
     :return:
     """
     global VERBOSE, PROBABILITY, SEND_WINDOW_SIZE, TIMEOUT
 
-    VERBOSE = opts.verbose
+    VERBOSE = options.verbose
 
-    _1, _2, _3 = [int(x) for x in opts.probability.split(":")]
+    _1, _2, _3 = [int(x) for x in options.probability.split(":")]
     if _1 + _2 + _3 == 100:
         PROBABILITY["成功"], PROBABILITY["误码"], PROBABILITY["丢包"] = _1, _2, _3
     else:
         print("概率参数设置错误, 将采取默认值-70:20:10")
 
-    if 0 < opts.size < MAX_FRAME_NUMBER:
-        SEND_WINDOW_SIZE = opts.size
+    if 0 < options.size < MAX_FRAME_NUMBER:
+        SEND_WINDOW_SIZE = options.size
     else:
         print("[*] 窗口大小参数错误, 采取默认值 3")
         SEND_WINDOW_SIZE = 3
 
-    TIMEOUT = opts.timeout
+    TIMEOUT = options.timeout
 
 
 if __name__ == "__main__":
