02eb71311a6f835c712f7c08e7edf6ed28b333b2
L1n <490772448@qq.com>
2016-06-22 10:29:26 +0800

    Python 面试题, 此题是关于单例模式的几种实现

---
+++ Python 面试题/Python 语言特性/7 单例模式/singleton_for_import.py
@@ -0,0 +1,18 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# version: Python3.X
+""" 作为被 import 的文件, 实现单例
+"""
+
+__author__ = '__L1n__w@tch'
+
+
+class MySingleton:
+    def foo(self):
+        pass
+
+
+my_singleton = MySingleton()
+
+if __name__ == "__main__":
+    pass
