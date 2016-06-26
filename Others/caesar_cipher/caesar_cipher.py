fa4c3185d3963c0e0920d9a4fc9555b32e333a43
L1n <490772448@qq.com>
2016-06-26 14:46:47 +0800

    给凯撒加了个解密操作, 跟加密有重复的代码, 是不是得优化一下

--- Others/caesar_cipher/caesar_cipher.py
+++ Others/caesar_cipher/caesar_cipher.py
@@ -10,7 +10,7 @@ __author__ = '__L1n__w@tch'
 def caesar_cipher_encrypt(plain_text, shift):
     """
     默认字典为: ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
-    :param plain_text: 待加密的铭文
+    :param plain_text: 待加密的明文
     :param shift: 偏移
     :return: str()
     """
@@ -22,6 +22,23 @@ def caesar_cipher_encrypt(plain_text, shift):
     for each_char in plain_text:
         cipher_text += map_dictionary[dictionary.index(each_char)]
     return cipher_text
+
+
+def caesar_cipher_decrypt(cipher_text, shift):
+    """
+    默认字典为: ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789
+    :param cipher_text: 待加密的明文
+    :param shift: 偏移
+    :return: str()
+    """
+    dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    shift %= len(dictionary)
+    map_dictionary = dictionary[shift:] + dictionary[:shift]
+
+    plain_text = str()
+    for each_char in cipher_text:
+        plain_text += dictionary[map_dictionary.index(each_char)]
+    return plain_text
 
 
 if __name__ == "__main__":
