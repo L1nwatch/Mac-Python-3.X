2b158f3747d1d7fe5d0c0eb4ea0cb4e50b822b9c
L1n <490772448@qq.com>
2016-07-12 19:47:35 +0800

    爬虫, 爬取某个收录大学专业对应学校的网站, 没写好, 待以后完善吧

--- Others/爬虫/get_school_list.py
+++ Others/爬虫/get_school_list.py
@@ -18,8 +18,8 @@ if __name__ == "__main__":
     # 以下是硬编码的
     # 读取网页源码同时把表格数据写入 tmp.txt 中
     with open(tmp_file, "w") as f:
-        for page in range(1, 44):
-            url = r"http://data.api.gkcx.eol.cn/soudaxue/querySchoolSpecialty.html?messtype=jsonp&zycengci=%E6%9C%AC%E7%A7%91&page={}&size=10&keyWord1=%E7%94%B5%E5%AD%90%E5%95%86%E5%8A%A1&province=&schooltype=&schoolprop=&callback=jQuery183041561147102699314_1467990873326&_=1467990886395".format(
+        for page in range(1, 127):
+            url = r"http://data.api.gkcx.eol.cn/soudaxue/querySchoolSpecialty.html?messtype=jsonp&zycengci=%E6%9C%AC%E7%A7%91&page={}&size=10&keyWord1=%E8%AE%A1%E7%AE%97%E6%9C%BA&province=&schooltype=&schoolprop=&callback=jQuery183008470115063210626_1468315203918&_=1468315205299".format(
                 page)
             response = s.get(url)
             text = response.content.decode("utf8")
