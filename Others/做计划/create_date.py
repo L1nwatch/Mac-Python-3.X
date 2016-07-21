31112c58e19df830ef9668608439ca35f0c1f53a
L1n <490772448@qq.com>
2016-07-21 11:11:17 +0800

    日常需要, 因为每次都得手动写日期, 干脆编了个程序来帮我打印出对应的日期

--- Others/做计划/create_date.py
+++ Others/做计划/create_date.py
@@ -3,23 +3,23 @@
 # version: Python3.X
 """ 平时自己做计划的格式类似如下:
 
-2016.06.29 周三:
+2016.07.21 周四:
     早上:
-        08:10~09:10:	Python 整理
-        09:10~10:10:	考试安排 + 微机原理
-
+        08:05~09:05:   尚未安排
+        09:05~10:05:   尚未安排
+        10:05~11:05:   尚未安排
+        11:05~11:30:   尚未安排
     下午:
-        13:30~14:30:
-        14:30~15:30:
-        15:30~16:30:
-        16:30~17:30:
-
+        13:31~14:31:   尚未安排
+        14:31~15:31:   尚未安排
+        15:31~16:31:   尚未安排
+        16:31~17:30:   尚未安排
     晚上:
-        18:30~19:30:
-        19:30~20:30:
-        20:30~21:30:
+        18:59~19:59:   尚未安排
+        19:59~20:59:   尚未安排
+        20:59~21:30:   尚未安排
 
-唯一的问题是每次都要手敲, 所以还是程序解决吧
+唯一的问题是每次都要手敲, 所以还是程序解决吧, 最终结果就是如上图所示
 """
 import datetime
 import calendar
@@ -40,6 +40,67 @@ def is_leap_year(year, learn=True):
         return calendar.isleap(year)
 
 
+def create_format_hour_plan(start_hour="08:10", end_hour="10:10", things_list=None):
+    """
+    创建格式化的时间表, 生成的形式类似于下面的:
+        08:10~09:10:	Python 整理
+        09:10~10:10:	考试安排 + 微机原理
+    :param start_hour: "08:10"
+    :param end_hour: "10:10"
+    :param things_list: ["Python 整理", "考试安排 + 微机原理"]
+    :return: ['08:10~09:10:   尚未安排', '09:10~10:10:   尚未安排']
+        '''
+        08:10~09:10:	Python 整理
+        09:10~10:10:	考试安排 + 微机原理
+        '''
+    """
+    hour_plan_list = list()
+
+    def __get_time_list(start, end):
+        """
+        给定一个开始时间和结束时间, 产生一个时间列表
+        :param start: 开始时间, 比如 "08:10"
+        :param end: 结束时间, 比如 "09:10"
+        :return: ["08:10~09:10", "09:10~10:10"]
+        """
+        a_list = list()
+
+        # 相关初始化
+        today = datetime.datetime.today()
+        year, month, day = today.year, today.month, today.day
+        int_start_hour, int_start_minute = [int(_) for _ in start.split(":")]
+        int_end_hour, int_end_minute = [int(_) for _ in end.split(":")]
+
+        # 产生如下的东西: 2016-07-21 08:10:00
+        start_date = datetime.datetime(year, month, day, int_start_hour, int_start_minute)
+        # 产生如下的东西: 2016-07-21 10:10:00
+        end_date = datetime.datetime(year, month, day, int_end_hour, int_end_minute)
+
+        # 生成各个时间段
+        while start_date < end_date:
+            next_time = start_date + datetime.timedelta(seconds=3600)
+            result = "{}:{}~".format(str(start_date.hour).zfill(2), str(start_date.minute).zfill(2))
+            if next_time <= end_date:
+                a_list.append(result + "{}:{}".format(str(next_time.hour).zfill(2), str(next_time.minute).zfill(2)))
+            else:
+                a_list.append(result + "{}:{}".format(str(end_date.hour).zfill(2), str(end_date.minute).zfill(2)))
+            start_date = next_time
+        return a_list
+
+    time_list = __get_time_list(start_hour, end_hour)
+
+    things_list = list()
+    # things_list 填充, 万一给的 things_list 长度不够就自动补充
+    if len(things_list) < len(time_list):
+        things_list.extend(["尚未安排" for i in range(len(time_list) - len(things_list))])
+
+    for time, things in zip(time_list, things_list):
+        time_format = "{}:   {}".format(time, things)
+        hour_plan_list.append(time_format)
+
+    return hour_plan_list
+
+
 def create_date(start_date=datetime.date.today(), number=15):
     """
     实现效果, 产生如下的字符串
@@ -51,19 +112,33 @@ def create_date(start_date=datetime.date.today(), number=15):
         '''
     :param start_date: 从哪个日期开始产生, 格式为 (2016,7,9,5), 表示 year=2016, month=7, day=9, week_day=5
     :param number: 要产生多少个字符串
-    :return: None
+    :return: ['2016.07.21 周四:', '2016.07.22 周五:', ... ]
     """
     week_days = ["一", "二", "三", "四", "五", "六", "日"]
+    date_list = list()
     date = start_date
 
     for i in range(number):
         year, month, day, week_day = date.year, date.month, date.day, date.weekday()
-        string = "{}.{}.{} 周{}:".format(year, str(month).zfill(2), str(day).zfill(2), week_days[week_day % 7])
-        print(string)
+        format_day = "{}.{}.{} 周{}:".format(year, str(month).zfill(2), str(day).zfill(2), week_days[week_day % 7])
+        date_list.append(format_day)
 
         # 更新下一天
         date += datetime.timedelta(days=1)
 
+    return date_list
+
 
 if __name__ == "__main__":
-    create_date()
+    date_plan = create_date()
+    hour_plan = [create_format_hour_plan("08:05", "11:30"), create_format_hour_plan("13:31", "17:30"),
+                 create_format_hour_plan("18:59", "21:30")]
+
+    with open("result.txt", "w") as f:
+        for each_day in date_plan:
+            print(each_day, file=f)
+            for each_time, each_hour_plan in zip(["早上", "下午", "晚上"], hour_plan):
+                print("    {}:".format(each_time), file=f)
+                for each_hour in each_hour_plan:
+                    print("        {}".format(each_hour), file=f)
+            print("", file=f)
