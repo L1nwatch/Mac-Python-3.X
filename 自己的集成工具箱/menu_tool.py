b9aaefba314ee0eb9690123d29917a286c6707da
L1n <490772448@qq.com>
2016-08-04 20:14:50 +0800

    更新了一下自己的工具箱, 原先登录校园网的密码是错误的

--- 自己的集成工具箱/menu_tool.py
+++ 自己的集成工具箱/menu_tool.py
@@ -129,7 +129,7 @@ def logging_school_wifi(verbose=False):
     else:
         # 认证页面
         url = "http://10.255.44.33:803/include/auth_action.php"
-        post_data = {"action": "login", "username": "13030110024", "password": "foreachlf",
+        post_data = {"action": "login", "username": "13030110024", "password": "5w8zPKpw4#cE*5woatwj",
                      "ac_id": "1", "save_me": 0}  # &user_ip=&nas_ip=&user_mac=&save_me=0&ajax=1"}
         response = requests.post(url, post_data)
 
