f9f84eb9c6344852c672d4e3ebf2ed2806d3c584
L1n <490772448@qq.com>
2016-08-13 15:12:42 +0800

    第一个 PyQt 跑成功了

---
+++ 文件内容搜索器/pyqt_main.py
@@ -0,0 +1,22 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# version: Python3.X
+"""
+自己的 ui 的 main 脚本
+"""
+
+import sys
+
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from pyqt_from_ui import *
+
+__author__ = '__L1n__w@tch'
+
+if __name__ == "__main__":
+    app = 0
+    app = QApplication(sys.argv)
+    mainWindow = QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(mainWindow)
+    mainWindow.show()
+    sys.exit(app.exec_())
