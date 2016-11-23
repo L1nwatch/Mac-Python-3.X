af516993ee910c77c8216c333e934ddc151eecac
L1n <490772448@qq.com>
2016-11-24 00:11:57 +0800

    新增了 2 个单元测试, 用来测试 json 解析以及创建目录树的

--- ATM案例爬虫/atm_scrapy.py
+++ ATM案例爬虫/atm_scrapy.py
@@ -57,6 +57,14 @@ class ATMScrapy:
         :param file_path:
         :return:
         """
+        return dict()
+
+    def create_dirs_from_dict(self, a_dict):
+        """
+        根据一个字典创建对应的文件夹
+        :param a_dict:
+        :return:
+        """
         pass
 
 
@@ -67,36 +75,3 @@ if __name__ == "__main__":
     atm_scrapy.crawl(atm_project_url)
     atm_scrapy.crawl(case_url)
     print("\u524d\u7f6e-\u767b\u5f55\u8bbe\u5907\u7ed5\u8fc7\u524d\u53f0\u52a0\u5bc6")
-
-"""
-分析数据:
-designer_ztree_2976_a   http://200.200.0.33/atm/projects/53c49025d105401f5e0003ec/usecases/582 92d13 d10540 0b46 000 0b1
-designer_ztree_2976_a   582 92d13 d10540 0b46 000 0b1
-designer_ztree_2977_a   582 3de25 d10540 182c 000 10d
-designer_ztree_2978_a   582 3e540 d10540 6c5e 000 090
-designer_ztree_2979_a   582 ac21b d10540 6b97 000 066
-designer_ztree_2980_a   582 aced0 d10540 795a 000 039
-designer_ztree_2981_a   582 c2d84 d10540 5b14 000 07e
-
-designer_ztree_1_a      http://200.200.0.33/atm/projects/53c49025d105401f5e0003ec/usecases/statistics?suite_id=55c96a08d105400f27067a23
-designer_ztree_1_a      55c96a08d105400f27067a23
-designer_ztree_2_a      55c96a75d105406a8a02d808
-designer_ztree_3_a      55c96a75d105400f411a1e73
-designer_ztree_2913_a   55c96a76d105400f411a1e74
-designer_ztree_2914_a   55c96a76d105400f411a1ebc
-designer_ztree_2915_a   57dbc983d10540692e000070
-designer_ztree_4_a      56946a1dd1054047b2000008
-designer_ztree_2916_a   56946a67d1054046d100000b
-designer_ztree_5_a      55c96a78d105400f411a1fd0
-designer_ztree_6_a      55c96a78d105400f411a1fd1
-designer_ztree_7_a      55c96a78d105400f411a1fd2
-designer_ztree_2917_a   55c96a79d105400f411a2016    前置-准备执行主机环境     9行          -- 66   ;len = 809
-designer_ztree_2918_a   55c96a79d105400f411a2058    134920TCP协议访问控制     7行         -- 35    ;len = 446
-designer_ztree_2919_a   55c96a79d105400f411a207b    134921UDP协议访问控制     11-12行      -- 46   ;len = 663
-designer_ztree_2920_a   55c96a79d105400f411a20a9    134915默认禁止原则        30-33行      -- 125  ;len = 1867
-designer_ztree_2921_a   55c96a79d105400f411a2126    134917目的IP访问控制       17 行       -- 87   ;len = 1203
-designer_ztree_2922_a   55c96a7ad105400f411a217d    134919目的端口访问控制      12 行        -- 62  ;len = 803
-designer_ztree_2923_a   55c96a7ad105400f411a21bb    134916源IP地址访问控制      17 行(18行为空) -- 85  ;len = 1133
-designer_ztree_2924_a   55c96a7ad105400f411a2210    134918源端口访问控制        8 行        -- 51   ;len = 559
-designer_ztree_2925_a   55c96a7ad105400f411a2243    后置-清理环境               10 行
-"""
--- ATM案例爬虫/test_unittest_atm_scrapy.py
+++ ATM案例爬虫/test_unittest_atm_scrapy.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 # version: Python3.X
 """
+2016.11.22 新增两个单元测试并且通过了, 分别是获取项目 id 以及下载 json 文件
 2016.11.20 作为爬虫的测试文件, 这里存放的是单元测试文件
 """
 import os
@@ -19,7 +20,7 @@ class UnittestATMScrapy(unittest.TestCase):
         self.test_atm_scrapy = ATMScrapy()
         self.test_project_id = "53c49025d105401f5e0003ec"
 
-    def test_can_get_case_names(self):
+    def test_can_download_json_file_from_project_id(self):
         """
         测试给定一个项目id, 能够下载到 json 文件并命名为 id.json
         :return:
@@ -58,7 +59,44 @@ class UnittestATMScrapy(unittest.TestCase):
         test_url = "http://200.200.0.33/atm/projects/{}/suites".format(test_project_id)
         self.assertEqual(self.test_atm_scrapy.get_project_id_from_url(test_url), test_project_id)
 
+    def test_can_parse_json_to_a_dict(self):
+        """
+        虽然 json 库已经帮我们封装好了操作, 不过这里还是要验证一下(虽然测试原则不让这么做)
+        :return:
+        """
+        a_dict = self.test_atm_scrapy.parse_tree_json_file("{}.json".format(self.test_project_id))
+        self.assertTrue(isinstance(a_dict, dict))
 
+        self.assertEqual(a_dict[0]["name"] == "必选发布测试项new")
+        self.assertEqual(a_dict[1]["name"] == "储运母盘测试(适用于32G SSD)")
+
+    def test_can_create_dir_from_a_dict(self):
+        """
+        现在已经有一个被 json 解析好的字典了, 验证是否能够从这个字典创建文件夹
+        :return:
+        """
+        a_dict = self.test_atm_scrapy.parse_tree_json_file("{}.json".format(self.test_project_id))
+        self.test_atm_scrapy.create_dirs_from_dict(a_dict)
+
+        parent_dirs = ["必选发布测试项new", "储运母盘测试(适用于32G SSD)"]
+        self.assertTrue(all(os.path.exists(os.path.join(os.curdir, parent_dir)) for parent_dir in parent_dirs))
+
+        children_dirs = ["前置", "新增异常处理机制", "国标测试", "IPv6功能-zwj", "新增20150908", "原发布测试项自动化", ""]
+        self.assertTrue(all(
+            os.path.exists(os.path.join(os.curdir, "储运母盘测试(适用于32G SSD)", children_dir)) for children_dir in
+            children_dirs))
+
+        test_dir = os.curdir
+        for next_dir in ["必选发布测试项new", "发布测试项", "国标测试", "功能列表", "包过滤"]:
+            test_dir = os.path.join(test_dir, next_dir)
+            self.assertTrue(os.path.exists(test_dir))
+
+    def test_can_get_all_cases_from_dir_id(self):
+        """
+        测试能够从一个文件夹的 id 获取到当前文件夹下的所有案例
+        :return:
+        """
+        pass
 
 
 if __name__ == "__main__":
