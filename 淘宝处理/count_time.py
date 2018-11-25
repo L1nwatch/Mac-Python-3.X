cbfbd13a068f02fdf1d6cfea030986a67bee6bce
L1n <490772448@qq.com>
2018-11-25 21:28:48 +0800

    写了个统计时间的脚本

---
+++ 淘宝处理/count_time.py
@@ -0,0 +1,18 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# version: Python3.X
+""" 计算上架时间
+"""
+import datetime
+
+__author__ = '__L1n__w@tch'
+
+if __name__ == "__main__":
+    start_time = datetime.datetime.strptime("2018-11-27 06:24:40", "%Y-%m-%d %H:%M:%S")
+    step = 1789
+    nums = 20
+
+    time = start_time
+    for i in range(20):
+        time += datetime.timedelta(seconds=step)
+        print(time)
