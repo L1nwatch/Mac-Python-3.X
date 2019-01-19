8a7bfbd273ae7c07a728009f815d47f7706753fd
L1n <490772448@qq.com>
2019-01-19 14:44:22 +0800

    写了个脚本方便自己上传图片到 谷歌

---
+++ 文件格式处理/all_sub_move_to_root.py
@@ -0,0 +1,17 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# version: Python3.X
+""" 把所有子目录照片放到 root 目录下
+"""
+import os
+import shutil
+
+
+__author__ = '__L1n__w@tch'
+
+if __name__ == "__main__":
+    root_path = r"/Users/L1n/Desktop/照片视频备份/计院毕业照"
+    dst_path = r"/Users/L1n/Desktop/照片视频备份/计院毕业照"
+    for root,dirs,files in os.walk(root_path):
+        for each_file in files:
+            shutil.move(os.path.join(root_path,root,each_file),os.path.join(dst_path,each_file))
