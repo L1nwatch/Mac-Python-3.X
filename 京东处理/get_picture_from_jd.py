333d36d4a37e3a6905fbf5191908f29e337823e1
L1n <490772448@qq.com>
2019-01-31 17:25:04 +0800

    好像详情图也可以爬下来了

--- 京东处理/get_picture_from_jd.py
+++ 京东处理/get_picture_from_jd.py
@@ -8,16 +8,46 @@ import re
 
 __author__ = '__L1n__w@tch'
 
-if __name__ == "__main__":
-    s = requests.session()
-    response = s.get("https://item.jd.com/100000378767.html")
+
+def get_picture(this_session):
+    """
+    获取主图
+    :return:
+    """
+    response = this_session.get("https://item.jd.com/100000378767.html")
     data = response.text
-    all_picture_url = re.findall("<li ><img alt='[^']*' src='[^']*'  data-url='([^']*)' data-img='1' width='50' height='50'></li>",data)
+    all_picture_url = re.findall(
+        "<li ><img alt='[^']*' src='[^']*'  data-url='([^']*)' data-img='1' width='50' height='50'></li>", data)
     all_picture_url = re.findall("<li ><img alt='.*", data)
     for i, each in enumerate(all_picture_url):
         picture_true_address = re.findall("data-url='([^']*)'", each)[0]
         picture_true_address_url = "https://img14.360buyimg.com/n1/s546x546_{}".format(picture_true_address)
         postfix = picture_true_address[picture_true_address.rfind("."):]
         response = s.get(picture_true_address_url)
-        with open("{}{}".format(i, postfix), "wb") as f:
+        with open("./主图/{}{}".format(i, postfix), "wb") as f:
             f.write(response.content)
+
+
+def get_detail(this_session):
+    """
+    获取详情图
+    :param this_session:
+    :return:
+    """
+    url = "https://cd.jd.com/description/channel?skuId=32789142913&mainSkuId=11983165386&cdn=2&callback=showdesc"
+    response = this_session.get(url)
+    data = response.text
+    # with open("test_detail.html", "r") as f:
+    #     data = f.read()
+    detail_picture = re.findall('data-lazyload=\\\\"//(.*)\\\\"', data)
+    for i, each_detail in enumerate(detail_picture):
+        response = this_session.get("http://{}".format(each_detail))
+        postfix = each_detail[each_detail.rfind("."):]
+        with open("./详情/{}{}".format(i, postfix), "wb") as f:
+            f.write(response.content)
+
+
+if __name__ == "__main__":
+    s = requests.session()
+    get_picture(s)
+    get_detail(s)
