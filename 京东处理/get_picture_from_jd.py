b2f89f45e7fe6e1155ffb38c0cb795058191647a
L1n <490772448@qq.com>
2019-01-31 15:56:06 +0800

    验证了几个商品,都没毛病

--- 京东处理/get_picture_from_jd.py
+++ 京东处理/get_picture_from_jd.py
@@ -10,16 +10,8 @@ __author__ = '__L1n__w@tch'
 
 if __name__ == "__main__":
     s = requests.session()
-    response = s.get("https://item.jd.com/32789142913.html#none")
+    response = s.get("https://item.jd.com/100000378767.html")
     data = response.text
-    # https://img14.360buyimg.com/n1/s546x546_jfs/t1/1708/9/2312/90314/5b96253bE4a4680b9/5623409d1acda299.jpg
-    # https://img13.360buyimg.com/n5/jfs/t1/1708/9/2312/90314/5b96253bE4a4680b9/5623409d1acda299.jpg
-    # jfs/t1/1708/9/2312/90314/5b96253bE4a4680b9/5623409d1acda299.jpg
-    # <li ><img alt='美逸 GT20PLUS20000毫安 移动电源 LCD屏显 双向快充QC3.0/2.0 带线 白骑士 白骑士' src='//img13.360buyimg.com/n5/jfs/t1/1708/9/2312/90314/5b96253bE4a4680b9/5623409d1acda299.jpg' data-url='jfs/t1/1708/9/2312/90314/5b96253bE4a4680b9/5623409d1acda299.jpg' data-img='1' width='50' height='50'></li>
-    # with open("test.html","w") as f:
-    #     f.write(response.text)
-    # with open("test.html") as f:
-    #     data = f.read()
     all_picture_url = re.findall("<li ><img alt='[^']*' src='[^']*'  data-url='([^']*)' data-img='1' width='50' height='50'></li>",data)
     all_picture_url = re.findall("<li ><img alt='.*", data)
     for i, each in enumerate(all_picture_url):
