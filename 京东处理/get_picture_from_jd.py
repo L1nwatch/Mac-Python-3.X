23cd9ac2677e5a74fa980c789c18ce1680ade26d
L1n <490772448@qq.com>
2019-02-01 16:11:19 +0800

    主图下载漏了一张,现在补上

--- 京东处理/get_picture_from_jd.py
+++ 京东处理/get_picture_from_jd.py
@@ -24,7 +24,7 @@ def get_picture(this_session, request_url):
     sku_id, main_sku_id = re.findall('qualityLife: "//c.3.cn/qualification/info\?skuId=(\d*)&pid=(\d*)', data)[0]
 
     # 下载主图
-    all_picture_url = re.findall("<li ><img alt='.*", data)
+    all_picture_url = re.findall("<img alt='.*", data)
     for i, each in enumerate(all_picture_url):
         picture_true_address = re.findall("data-url='([^']*)'", each)[0]
         picture_true_address_url = "https://img14.360buyimg.com/n1/s546x546_{}".format(picture_true_address)
