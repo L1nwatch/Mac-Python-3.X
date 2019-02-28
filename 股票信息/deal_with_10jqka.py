e9c1c9de601f46b5033ceb98cc3e2612ec5ac68b
L1n <490772448@qq.com>
2019-02-28 08:09:25 +0800

    股票信息更新

--- 股票信息/deal_with_10jqka.py
+++ 股票信息/deal_with_10jqka.py
@@ -22,13 +22,16 @@ __author__ = '__L1n__w@tch'
 browser = None
 this_month = datetime.datetime.today().strftime("%Y-%m")
 this_month_log_path = "./stock_log/{}".format(this_month)
-this_day_log_path = "./stock_log/{}/{}_result.txt".format(this_month, datetime.datetime.today().strftime("%Y-%m-%d"))
+today = datetime.datetime.today().strftime("%Y-%m-%d")
+this_day_log_path_day = "./stock_log/{}/{}_result_day.txt".format(this_month, today)
+this_day_log_path_week = "./stock_log/{}/{}_result_week.txt".format(this_month, today)
 
 
-def get_prices_using_number(number):
+def get_prices_using_number(number, day_step=True):
     """
     根据股票代码,打印出日 K 线里面最低、最高、当前价格
     :param number:
+    :param day_step:
     :return:
     """
     global browser
@@ -49,9 +52,14 @@ def get_prices_using_number(number):
     browser.get("http://stockpage.10jqka.com.cn/HQ_v4.html#{}".format(number))
     time.sleep(1)
     for each_element in browser.find_elements_by_class_name("kline-type"):
-        if each_element.text == "日K线":
-            each_element.click()
-            break
+        if day_step:
+            if each_element.text == "日K线":
+                each_element.click()
+                break
+        else:
+            if each_element.text == "周K线":
+                each_element.click()
+                break
 
     while "hxc3-klineprice-min" not in browser.page_source:
         time.sleep(1)
@@ -87,37 +95,48 @@ def get_all_number():
     return result
 
 
-def get_prices():
+def get_prices(day_step=True):
     """
     获取所有业绩预告的股票代码,并获取对应的当前值、最高值、最低值
+    :param day_step:
     :return:
     """
+    if day_step:
+        log_path = this_day_log_path_day
+    else:
+        log_path = this_day_log_path_week
+
     global browser
     browser = webdriver.Chrome(
         "/Users/L1n/Desktop/Code/Python/my_blog_source/virtual/selenium/webdriver/chromedriver",
     )
 
     # finish_list = list()
-    # with open(this_day_log_path) as f:
+    # with open(log_path) as f:
     #     for each_line in f:
     #         finish_list.append(each_line[13:19])
 
     numbers = get_all_number()
     os.makedirs(this_month_log_path, exist_ok=True)
-    with open(this_day_log_path, "w") as f:
+    with open(log_path, "w") as f:
         for each_number in numbers:
             # if each_number in finish_list:
             #     continue
-            string = get_prices_using_number("hs_{}".format(each_number))
+            string = get_prices_using_number("hs_{}".format(each_number), day_step)
             if string:
                 print(string, file=f, flush=True)
 
     browser.quit()
 
 
-def analysis_prices():
+def analysis_prices(day_step=True):
+    if day_step:
+        log_path = this_day_log_path_day
+    else:
+        log_path = this_day_log_path_week
+
     data = list()
-    with open(this_day_log_path) as f:
+    with open(log_path) as f:
         for each_line in f:
             number, cur_price, high_price, low_price = each_line.split(",")
             number = number[13:]
@@ -129,7 +148,7 @@ def analysis_prices():
             data.append(info)
     data = sorted(data, key=lambda x: x["sep"])
 
-    with open(this_day_log_path, "a") as f:
+    with open(log_path, "a") as f:
         print("\n[!] {sep} 开始过滤 {sep}\n".format(sep="=" * 30), file=f)
 
         for each_data in data:
@@ -143,5 +162,11 @@ def analysis_prices():
 
 
 if __name__ == "__main__":
+    print("[*] {sep} 先获取日 K 线的结果 {sep}".format(sep="=" * 30))
     get_prices()
     analysis_prices()
+
+    print("[*] {sep} 再获取周 K 线的结果 {sep}".format(sep="=" * 30))
+
+    get_prices(day_step=False)
+    analysis_prices(day_step=False)
