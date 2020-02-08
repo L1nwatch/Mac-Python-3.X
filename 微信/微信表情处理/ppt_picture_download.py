3d775255111ca9ecf98894997968cdd7981952fc
L1n <490772448@qq.com>
2020-02-08 09:20:26 +0800

    加一个 PPT 下载脚本

---
+++ 微信/微信表情处理/ppt_picture_download.py
@@ -0,0 +1,15 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+# version: Python3.X
+""" Description
+"""
+import requests
+
+__author__ = '__L1n__w@tch'
+
+if __name__ == "__main__":
+    for i in range(30):
+        url = "https://img.lycheer.net/ppt/31665998/1580814719089-0000{}.png/ppt1280q70".format(str(i).zfill(2))
+        with open("{}.png".format(i), "wb") as f:
+            response = requests.get(url)
+            f.write(response.content)
